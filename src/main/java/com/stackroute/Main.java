1144f511493eabf5098091536870363f209cde2f
amit-s-k <amitsk2014@gmail.com>
2019-01-16 12:13:08 +0530

    Refactored

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,7 +1,6 @@
 package com.stackroute;
 
 import com.stackroute.config.AppConfig;
-import com.stackroute.domain.HelloWorld;
 import com.stackroute.domain.Movie;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
@@ -11,10 +10,6 @@ public class Main {
         AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
         context.register(AppConfig.class);
         context.refresh();
-        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
-        System.out.println(helloWorld.getMessage());
-
-
         Movie movie = (Movie) context.getBean("movie");
         movie.log();
     }
