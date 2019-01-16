c82de9cf64b83402797ceae9207bbd3cddd986bd
amit-s-k <amitsk2014@gmail.com>
2019-01-16 10:31:47 +0530

    Bean configurations using Annotations

--- src/main/java/com/stackroute/Main.java
+++ src/main/java/com/stackroute/Main.java
@@ -1,40 +1,16 @@
 package com.stackroute;
 
+import com.stackroute.config.AppConfig;
 import com.stackroute.domain.HelloWorld;
-import org.springframework.beans.factory.BeanFactory;
-import org.springframework.beans.factory.support.BeanDefinitionReader;
-import org.springframework.beans.factory.support.BeanDefinitionRegistry;
-import org.springframework.beans.factory.support.DefaultListableBeanFactory;
-import org.springframework.beans.factory.xml.XmlBeanDefinitionReader;
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-import org.springframework.core.io.ClassPathResource;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class Main {
-    public static void main(String[] args){;
-        HelloWorld helloWorld=new HelloWorld();
+    public static void main(String[] args) {
+
+        AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext();
+        context.register(AppConfig.class);
+        context.refresh();
+        HelloWorld helloWorld = (HelloWorld) context.getBean("helloWorld");
         System.out.println(helloWorld.getMessage());
-
-        BeanFactory beanFactory=new XmlBeanFactory(new ClassPathResource("beans.xml"));
-        HelloWorld helloWorld1=(HelloWorld) beanFactory.getBean("helloWorld");
-        System.out.println(helloWorld1.getMessage());
-
-        BeanDefinitionRegistry beanDefinitionRegistry=new DefaultListableBeanFactory();
-        BeanDefinitionReader beanDefinitionReader=new XmlBeanDefinitionReader(beanDefinitionRegistry);
-        beanDefinitionReader.loadBeanDefinitions(new ClassPathResource("beans.xml"));
-        HelloWorld helloWorld2=((DefaultListableBeanFactory) beanDefinitionRegistry).getBean(HelloWorld.class);
-        System.out.println(helloWorld2.getMessage());
-
-        ApplicationContext context=new ClassPathXmlApplicationContext("beans.xml");
-        HelloWorld helloWorld3=context.getBean(HelloWorld.class);
-        System.out.println(helloWorld3.getMessage());
-
-
-
-
     }
-
-
-
 }
--- src/main/java/com/stackroute/domain/HelloWorld.java
+++ src/main/java/com/stackroute/domain/HelloWorld.java
@@ -1,9 +1,11 @@
 package com.stackroute.domain;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class HelloWorld {
     private String msg="helloworld";
     public String getMessage(){
         return msg;
     }
-
 }
